99b110e8788a94c1628c24371c568f3ed694e58a
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-03-22 11:54:30 +0600

    HAVING  key added

--- groupBy.sql
+++ groupBy.sql
@@ -14,3 +14,16 @@ SELECT country, AVG(age) FROM students
     GROUP BY country
     HAVING AVG(age) > 20;
 
+
+-- Filter Groups using HAVING to show only countries with avarage age above 22 .
+
+SELECT country, AVG(age) FROM students
+GROUP BY country
+HAVING AVG(age) > 22;
+
+-- Count student Born in each year 
+
+SELECT EXTRACT(YEAR FROM dob) as birth_year, COUNT(*) 
+FROM students
+GROUP BY birth_year;
+
